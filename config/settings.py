91aea4db723b6e8eab79223594f3e7f962d748c9
jointresearchshun-tech <joint.research.shun@gmail.com>
2025-09-02 16:20:54 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -5,3 +5,6 @@ SUPPORTED_UPLOAD_TYPES = [
 "pt", "pth", "zip", "png", "jpg", "jpeg", "csv", "txt", "json"
 ]
 PROCESSING_TYPES = ["hologram_processing", "image_analysis", "custom_processing"]
+
+DATA_FOLDER = "data"
+RESULTS_FOLDER = "results"
