a6f6dfe0c91260bdab65a91615084e17b3f136ff
jointresearchshun-tech <joint.research.shun@gmail.com>
2025-09-02 16:16:10 +0900

    Create settings.py

---
+++ config/settings.py
@@ -0,0 +1,7 @@
+# Shared constants & defaults
+DEFAULT_DATA_FOLDER = "data"
+DEFAULT_RESULTS_FOLDER = "results"
+SUPPORTED_UPLOAD_TYPES = [
+"pt", "pth", "zip", "png", "jpg", "jpeg", "csv", "txt", "json"
+]
+PROCESSING_TYPES = ["hologram_processing", "image_analysis", "custom_processing"]
