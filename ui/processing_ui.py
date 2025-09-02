fabbb78e0e04c2588100454a1dc978e255e3952e
jointresearchshun-tech <joint.research.shun@gmail.com>
2025-09-02 16:27:16 +0900

    Create processing_ui.py

---
+++ ui/processing_ui.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def processing_ui():
+    st.subheader("⚡ Processing")
+    st.write("Send files to Colab servers for processing.")
+    # TODO: implement job submission form
