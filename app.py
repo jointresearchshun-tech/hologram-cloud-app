6a4b2f03ae0cc9347a716045ead3e5c3a4a43400
jointresearchshun-tech <joint.research.shun@gmail.com>
2025-09-02 16:43:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import streamlit as st
+from config.logging_config import setup_logging
 
 def main():
-    st.write("Hello from minimal app.py")
+    setup_logging()
+    st.write("Hello with logging_config import")
 
 if __name__ == "__main__":
     main()
